a14ec979983ab0b4f0392324dd465cc75a9d6718
Tim-1e <xy_zml@foxmail.com>
2026-07-23 16:04:19 +0800

    refactor: remove unused sample compatibility code

--- src/sutil/Preprocessor.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <renderer/Preprocessor.h>
--- src/viewer/sutil/sutilapi.h
+++ src/viewer/sutil/sutilapi.h
@@ -29,37 +29,9 @@
 #pragma once
 
 #ifndef SUTILAPI
-#  if sutil_7_sdk_EXPORTS /* Set by CMAKE */
-#    if defined( _WIN32 ) || defined( _WIN64 )
-#      define SUTILAPI __declspec(dllexport)
-#      define SUTILCLASSAPI
-#    elif defined( linux ) || defined( __linux__ ) || defined ( __CYGWIN__ )
-#      define SUTILAPI __attribute__ ((visibility ("default")))
-#      define SUTILCLASSAPI SUTILAPI
-#    elif defined( __APPLE__ ) && defined( __MACH__ )
-#      define SUTILAPI __attribute__ ((visibility ("default")))
-#      define SUTILCLASSAPI SUTILAPI
-#    else
-#      error "CODE FOR THIS OS HAS NOT YET BEEN DEFINED"
-#    endif
+#  define SUTILAPI
+#endif
 
-#  else /* sutil_7_sdk_EXPORTS */
-
-#    if defined( _WIN32 ) || defined( _WIN64 )
-#      define SUTILAPI __declspec(dllimport)
-#      define SUTILCLASSAPI
-#    elif defined( linux ) || defined( __linux__ ) || defined ( __CYGWIN__ )
-#      define SUTILAPI __attribute__ ((visibility ("default")))
-#      define SUTILCLASSAPI SUTILAPI
-#    elif defined( __APPLE__ ) && defined( __MACH__ )
-#      define SUTILAPI __attribute__ ((visibility ("default")))
-#      define SUTILCLASSAPI SUTILAPI
-#    elif defined( __CUDACC_RTC__ )
-#      define SUTILAPI
-#      define SUTILCLASSAPI
-#    else
-#      error "CODE FOR THIS OS HAS NOT YET BEEN DEFINED"
-#    endif
-
-#  endif /* sutil_7_sdk_EXPORTS */
+#ifndef SUTILCLASSAPI
+#  define SUTILCLASSAPI
 #endif
